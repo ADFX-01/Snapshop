f86f57196e2c48d82ee6a31ae12e81bb4ff66991
ytaebnek <43914827+ytaebnek@users.noreply.github.com>
2018-10-07 00:19:27 -0400

    Update GoogleSearch.py

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -8,7 +8,7 @@ with open('tags.txt', 'r') as myfile:
     tags = myfile.read().replace('\n', '')
 
 searchTerms = tags + " , shopping"
-print(searchTerms)
+
 print("Top result(s) pertaining to your image search: ")
 for j in search(searchTerms, tld="com", num=1, stop=1, pause=2):
     print(j)
