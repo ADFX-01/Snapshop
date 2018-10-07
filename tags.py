cbe754981380b256a22a6c638bb4979e68123aba
Ban Khuc <bankhuc@gmail.com>
2018-10-06 20:52:02 -0400

    Add files via upload

---
+++ tags.py
@@ -0,0 +1,77 @@
+from google.cloud import vision
+import os
+import io
+
+#find current directory
+cwd = os.getcwd()
+api_key = cwd + "\quikShop.json"
+image = cwd + "\image1.jpg"
+
+#set api_key
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = api_key
+
+global tags
+tags = ''
+
+def detect_logos(path):
+    """Detects logos in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    global tags
+
+    for logo in logos:
+        tags = tags + logo.description + ', ' 
+
+def detect_labels(path):
+    """Detects labels in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('Labels:')
+
+    global tags
+
+    for label in labels:
+        tags = tags + label.description + ', ' 
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Labels:')
+
+    global tags
+
+    for text in texts:
+        tags = tags + text.description + ', ' 
+
+detect_labels(image)
+detect_logos(image)
+detect_text(image)
+
+print(tags)
