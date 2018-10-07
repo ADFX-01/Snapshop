b0138a9d9dc19ae11e4f87c05eeea2ae98247aac
Ban Khuc <bankhuc@gmail.com>
2018-10-06 22:25:13 -0400

    Add files via upload

--- tags.py
+++ tags.py
@@ -75,3 +75,12 @@ detect_logos(image)
 detect_text(image)
 
 print(tags)
+
+from googlesearch import search
+
+tags = "samsung, s8, technology, phone"
+searchTerms = tags + " , shopping"
+
+print("Top result(s) pertaining to your image search: ")
+for j in search(searchTerms, tld="com", num=1, stop=1, pause=2):
+    print(j)
